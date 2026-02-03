45b9a5dfe4f86ee19af572b0adfc1e9bd8031fc4
AroFortunat <aroralaiseheno@gmail.com>
2026-02-03 15:03:45 +0300

    feat : add button dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,8 @@ import {
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Link from "next/link";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,35 +35,43 @@ export default function RootLayout({
 }>) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en"suppressHydrationWarning>
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <header className="w-full">
-            <div className="w-full max-w-3/4 flex items-center justify-between mx-auto p-4">
-              <Link href="/">
-                <h3 className="text-2xl font-bold">Discord Clone App</h3>
-              </Link>
-              <div className="flex items-center gap-4">
-                <SignedOut>
-                  <SignInButton>
-                    <button className="border rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                      Sign In
-                    </button>
-                  </SignInButton>
-                  <SignUpButton>
-                    <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
-                      Sign Up
-                    </button>
-                  </SignUpButton>
-                </SignedOut>
-                <SignedIn>
-                  <UserButton />
-                </SignedIn>
+          <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          storageKey="discord-theme"
+          >
+            <header className="w-full">
+              <div className="w-full max-w-3/4 flex items-center justify-between mx-auto p-4">
+                <Link href="/">
+                  <h3 className="text-2xl font-bold">Discord Clone App</h3>
+                </Link>
+                <div className="flex items-center gap-4">
+                  <ModeToggle/> 
+                  <SignedOut>
+                    <SignInButton>
+                      <button className="border rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                        Sign In
+                      </button>
+                    </SignInButton>
+                    <SignUpButton>
+                      <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                        Sign Up
+                      </button>
+                    </SignUpButton>
+                  </SignedOut>
+                  <SignedIn>
+                    <UserButton />
+                  </SignedIn>
+                </div>
               </div>
-            </div>
-          </header>
-          {children}
+            </header>
+            {children}
+          </ThemeProvider>
         </body>
       </html>
     </ClerkProvider>
