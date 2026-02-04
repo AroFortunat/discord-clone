7886988b1e58b53e9091c68ded37733cb9c024d6
AroFortunat <aroralaiseheno@gmail.com>
2026-02-04 16:30:44 +0300

    update: update folder structure

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,8 @@
+import { initialProfile } from "@/lib/initial-profile";
+
+const SetupPage = async () => {
+    const profile = await initialProfile()
+    return <div>Create a Server </div>;
+};
+
+export default SetupPage;
